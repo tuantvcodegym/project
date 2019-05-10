5b08e959209dcfc1f2bee551241a8db2abb6a3ab
Tuan Tran <tuank11htt@gmail.com>
2019-05-10 11:49:49 +0700

    fix image

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,5 +31,6 @@ class AppServiceProvider extends ServiceProvider
             // share du lieu cho nhieu tat ca cac view ma extends view master
             $view->with('cate',$cate);
         });
+        URL::forceScheme('https');
     }
 }
